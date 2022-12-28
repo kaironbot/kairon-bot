cadd8bb00222ef40420bc9d3e1557d3dec9d6fbf
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-12-28 12:21:42 +0100

    Added response event

--- src/main/kotlin/org/wagham/events/WaghamFunnyResponseEvent.kt
+++ src/main/kotlin/org/wagham/events/WaghamFunnyResponseEvent.kt
@@ -8,8 +8,9 @@ import kotlinx.datetime.toLocalDateTime
 import org.wagham.annotations.BotEvent
 import org.wagham.components.CacheManager
 import org.wagham.db.KabotMultiDBClient
+import org.wagham.utils.uniformProbability
 
-@BotEvent
+@BotEvent("wagham")
 class WaghamFunnyResponseEvent(
     override val kord: Kord,
     override val db: KabotMultiDBClient,
@@ -17,24 +18,25 @@ class WaghamFunnyResponseEvent(
 ) : Event {
 
     override val name = "wagham_funny_response_event"
+
     override fun register() {
         kord.on<MessageCreateEvent> {
             when {
                 Regex("non +vedo +l.ora").matches(message.content.lowercase()) ->
                     "Sono le ${message.timestamp.toLocalDateTime(TimeZone.UTC).hour}:${message.timestamp.toLocalDateTime(TimeZone.UTC).minute}"
-                Regex("chiudi +la +bocca").matches(message.content.lowercase()) ->
+                uniformProbability(40) && Regex("chiudi +la +bocca").matches(message.content.lowercase()) ->
                     "Zitto coglione!"
-                Regex("jack").matches(message.content.lowercase()) ->
+                uniformProbability(15) && Regex("jack").matches(message.content.lowercase()) ->
                     "Mentecatto!"
-                Regex("ma +io +sono +l.eroe").matches(message.content.lowercase()) ->
+                uniformProbability(85) && Regex("ma +io +sono +l.eroe").matches(message.content.lowercase()) ->
                     "CLB è l'eroe!"
-                Regex("goblin +di +merda").matches(message.content.lowercase()) ->
+                uniformProbability(85) && Regex("goblin +di +merda").matches(message.content.lowercase()) ->
                     "Sì, il goblin fa schifo!"
-                Regex("chi +mi +ha +colpito").matches(message.content.lowercase()) ->
+                uniformProbability(85) && Regex("chi +mi +ha +colpito").matches(message.content.lowercase()) ->
                     "È stata l'Alta Stella!"
-                Regex("l.alta stella").matches(message.content.lowercase()) ->
+                uniformProbability(75) && Regex("l.alta stella").matches(message.content.lowercase()) ->
                     "Quella buona e quella bella"
-                Regex("gilda +di +wagham").matches(message.content.lowercase()) ->
+                uniformProbability(50) && Regex("gilda +di +wagham").matches(message.content.lowercase()) ->
                     "Assassini diplomatici"
                 else -> null
             }?.let {
