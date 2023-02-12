03653ad48eea8b401e34e8f3a0cca46362c13bce
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-02-12 12:23:08 +0100

    Updated Kotlin version and db library

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.8.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
@@ -36,7 +36,7 @@ dependencies {
     implementation(group="dev.kord", name="kord-core", version="0.8.0-M17")
     implementation(group="org.jetbrains.kotlinx", name="kotlinx-coroutines-core", version="1.6.4")
     implementation(group="org.jetbrains.kotlinx", name="kotlinx-coroutines-reactor", version="1.6.4")
-    implementation(group="org.wagham", name="kabot-db-connector", version="0.6.1")
+    implementation(group="org.wagham", name="kabot-db-connector", version="0.7.0")
     implementation(group="com.fasterxml.jackson.module", name="jackson-module-kotlin", version="2.13.4")
     implementation(group="org.slf4j", name="slf4j-api", version = "2.0.5")
     implementation(group="org.slf4j", name="slf4j-simple", version = "2.0.5")
