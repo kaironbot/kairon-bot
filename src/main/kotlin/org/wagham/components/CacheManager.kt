857dfd357ed09aff4a498fb6902100269746636c
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-08-05 09:22:20 +0200

    Small fix

--- src/main/kotlin/org/wagham/components/CacheManager.kt
+++ src/main/kotlin/org/wagham/components/CacheManager.kt
@@ -43,8 +43,6 @@ class CacheManager(
     suspend fun getExpTable(guildId: Snowflake): ExpTable =
         expTableCache.getIfPresent(guildId) ?:
             db.utilityScope.getExpTable(guildId.toString()).also {
-                println(guildId)
-                println(it)
                 expTableCache.put(guildId, it)
             }
 
