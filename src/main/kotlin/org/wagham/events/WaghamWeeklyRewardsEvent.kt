51f75d5dc82941860834c2fdcb238ad9121d3dd8
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-10-01 20:06:43 +0200

    Small fixes

--- src/main/kotlin/org/wagham/events/WaghamWeeklyRewardsEvent.kt
+++ src/main/kotlin/org/wagham/events/WaghamWeeklyRewardsEvent.kt
@@ -238,8 +238,8 @@ class WaghamWeeklyRewardsEvent(
 
     override fun register() {
         Timer(eventId).schedule(
-            getStartingInstantOnNextDay(18, 0, 0){
-                it.with(TemporalAdjusters.next(DayOfWeek.TUESDAY))
+            getStartingInstantOnNextDay(18, 0, 0) {
+                it.minusDays(3).with(TemporalAdjusters.next(DayOfWeek.TUESDAY))
             }.also {
                 logger.info { "$eventId will start on $it"  }
             },
