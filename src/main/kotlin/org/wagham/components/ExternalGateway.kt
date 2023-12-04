144acd56de4c94e975f30cffc1f9a8f0b35e7429
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-12-04 22:02:26 +0100

    Improved Loggin on AssignItemOnLevelUpEvent (#101)
    * Improved feedback on item craft
    
    * Small fix to report
    
    * Improved logging

--- src/main/kotlin/org/wagham/components/ExternalGateway.kt
+++ src/main/kotlin/org/wagham/components/ExternalGateway.kt
@@ -41,6 +41,8 @@ class ExternalGateway(
     private fun Routing.levelUpHandler() = post("/levelUp") {
         println("Received LevelUp")
         val update = call.receive<ExpUpdate>()
+        println("Guild id: ${update.guildId}")
+        println("Characters: ${update.updates.keys.joinToString(", ")}")
         cacheManager.sendToChannel<AssignItemOnLevelUpEvent, ExpUpdate>(update)
         call.respond("ok")
     }
