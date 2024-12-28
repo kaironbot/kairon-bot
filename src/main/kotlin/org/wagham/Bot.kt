5b91f2aed4b45c975bc2507800877446501d7a4e
Vincenzo Pierro <vcp@icure.com>
2024-12-28 09:38:05 +0100

    Removed commented code for testing

--- src/main/kotlin/org/wagham/Bot.kt
+++ src/main/kotlin/org/wagham/Bot.kt
@@ -29,131 +29,131 @@ import org.wagham.events.Event
 import kotlin.reflect.full.primaryConstructor
 
 class KaironBot(
-    private val profile: String,
-    private val kord: Kord
+	private val profile: String,
+	private val kord: Kord
 ) {
 
-    private val database = KabotMultiDBClient(
-        MongoCredentials(
-            "ADMIN",
-            System.getenv("DB_ADMIN_USER"),
-            System.getenv("DB_ADMIN_PWD"),
-            System.getenv("DB_ADMIN_NAME"),
-            System.getenv("DB_ADMIN_IP"),
-            System.getenv("DB_ADMIN_PORT").toInt()
-        )
-    )
-    private val schedulingManager = SchedulingManager(database)
-    private val cacheManager = CacheManager(database, schedulingManager, profile)
-    private val logger = KotlinLogging.logger {}
-    private val commands: List<Command<RequestBuilder<*>>>
-    private val events: List<Event>
+	private val database = KabotMultiDBClient(
+		MongoCredentials(
+			"ADMIN",
+			System.getenv("DB_ADMIN_USER"),
+			System.getenv("DB_ADMIN_PWD"),
+			System.getenv("DB_ADMIN_NAME"),
+			System.getenv("DB_ADMIN_IP"),
+			System.getenv("DB_ADMIN_PORT").toInt()
+		)
+	)
+	private val schedulingManager = SchedulingManager(database)
+	private val cacheManager = CacheManager(database, schedulingManager, profile)
+	private val logger = KotlinLogging.logger {}
+	private val commands: List<Command<RequestBuilder<*>>>
+	private val events: List<Event>
 
-    private fun autowireCommands() = Reflections("org.wagham.commands")
-        .getTypesAnnotatedWith(BotCommand::class.java)
-        .map { it.kotlin }
-        .filter {
-            it.annotations.any { ann ->
-                ann is BotCommand && (ann.profile == "all" || ann.profile == profile)
-            }
-        }
-        .map {
-            @Suppress("UNCHECKED_CAST")
-            it.primaryConstructor!!.call(kord, database, cacheManager) as Command<RequestBuilder<*>>
-        }
+	private fun autowireCommands() = Reflections("org.wagham.commands")
+		.getTypesAnnotatedWith(BotCommand::class.java)
+		.map { it.kotlin }
+		.filter {
+			it.annotations.any { ann ->
+				ann is BotCommand && (ann.profile == "all" || ann.profile == profile)
+			}
+		}
+		.map {
+			@Suppress("UNCHECKED_CAST")
+			it.primaryConstructor!!.call(kord, database, cacheManager) as Command<RequestBuilder<*>>
+		}
 
-    private fun autowireEvents() = Reflections("org.wagham.events")
-        .getTypesAnnotatedWith(BotEvent::class.java)
-        .map { it.kotlin }
-        .filter {
-            it.annotations.any { ann ->
-                ann is BotEvent && (ann.profile == "all" || ann.profile == profile)
-            }
-        }
-        .map {
-            it.primaryConstructor!!.call(kord, database, cacheManager) as Event
-        }
+	private fun autowireEvents() = Reflections("org.wagham.events")
+		.getTypesAnnotatedWith(BotEvent::class.java)
+		.map { it.kotlin }
+		.filter {
+			it.annotations.any { ann ->
+				ann is BotEvent && (ann.profile == "all" || ann.profile == profile)
+			}
+		}
+		.map {
+			it.primaryConstructor!!.call(kord, database, cacheManager) as Event
+		}
 
-    init {
-        commands = autowireCommands().map {
-            cacheManager.registerCommand(it.commandName)
-            it
-        }
-        events = autowireEvents()
+	init {
+		commands = autowireCommands().map {
+			cacheManager.registerCommand(it.commandName)
+			it
+		}
+		events = autowireEvents()
 
-        kord.on<ReadyEvent> {
-            kord.getGlobalApplicationCommands(true).collect {
-                if(!cacheManager.getCommands().contains(it.name)) {
-                    it.delete()
-                    logger.info { "Deleting ${it.name} command" }
-                }
-            }
-            supplier.guilds.collect {
-                database.serverConfigScope.getGuildConfig(it.id.toString()).channels[Channels.LOG_CHANNEL.name]?.let { channelId ->
-                    supplier.getChannel(Snowflake(channelId)).asChannelOf<MessageChannel>().createMessage {
-                        content = "KaironBot started!"
-                    }
-                } ?: it.getSystemChannel()
-                    ?.createMessage {
-                        content = "KaironBot started! To change the logging channel, use the /set_channel command"
-                    }
-            }
-            schedulingManager.retrieveInterruptedTasks(supplier.guilds)
-        }
-    }
+		kord.on<ReadyEvent> {
+			kord.getGlobalApplicationCommands(true).collect {
+				if(!cacheManager.getCommands().contains(it.name)) {
+					it.delete()
+					logger.info { "Deleting ${it.name} command" }
+				}
+			}
+			supplier.guilds.collect {
+				database.serverConfigScope.getGuildConfig(it.id.toString()).channels[Channels.LOG_CHANNEL.name]?.let { channelId ->
+					supplier.getChannel(Snowflake(channelId)).asChannelOf<MessageChannel>().createMessage {
+						content = "KaironBot started!"
+					}
+				} ?: it.getSystemChannel()
+					?.createMessage {
+						content = "KaironBot started! To change the logging channel, use the /set_channel command"
+					}
+			}
+			schedulingManager.retrieveInterruptedTasks(supplier.guilds)
+		}
+	}
 
-    @OptIn(PrivilegedIntent::class)
-    suspend fun start() = coroutineScope {
-        logger.info { "Starting KaironBot with profile $profile" }
-        events.forEach {
-            it.register()
-            cacheManager.registerEvent(it.eventId)
-            logger.info { "Registered ${it.eventId} event" }
-        }
-//        commands.forEach {
-//            it.registerCommand()
-//            it.registerCallback()
-//            logger.info { "Registered ${it.commandName} command" }
-//        }
+	@OptIn(PrivilegedIntent::class)
+	suspend fun start() = coroutineScope {
+		logger.info { "Starting KaironBot with profile $profile" }
+		events.forEach {
+			it.register()
+			cacheManager.registerEvent(it.eventId)
+			logger.info { "Registered ${it.eventId} event" }
+		}
+		commands.forEach {
+			it.registerCommand()
+			it.registerCallback()
+			logger.info { "Registered ${it.commandName} command" }
+		}
 
-        launch { schedulingManager.launchTasks(cacheManager, kord) }
+		launch { schedulingManager.launchTasks(cacheManager, kord) }
 
-        cacheManager.createNewCollectionCache<BuildingWithBounty> { guildId, db ->
-            db.buildingsScope.getBuildingsWithBounty(guildId.toString()).toList()
-        }
+		cacheManager.createNewCollectionCache<BuildingWithBounty> { guildId, db ->
+			db.buildingsScope.getBuildingsWithBounty(guildId.toString()).toList()
+		}
 
-        cacheManager.createNewCollectionCache<BuildingRecipe> { guildId, db ->
-            db.buildingsScope.getAllBuildingRecipes(guildId.toString()).toList()
-        }
+		cacheManager.createNewCollectionCache<BuildingRecipe> { guildId, db ->
+			db.buildingsScope.getAllBuildingRecipes(guildId.toString()).toList()
+		}
 
-        cacheManager.createNewCollectionCache<Item> { guildId, db ->
-            db.itemsScope.getAllItems(guildId.toString()).toList().sortedBy { it.name }
-        }
+		cacheManager.createNewCollectionCache<Item> { guildId, db ->
+			db.itemsScope.getAllItems(guildId.toString()).toList().sortedBy { it.name }
+		}
 
-        cacheManager.createNewCollectionCache<LanguageProficiency> { guildId, db ->
-            db.proficiencyScope.getLanguages(guildId.toString()).toList()
-        }
+		cacheManager.createNewCollectionCache<LanguageProficiency> { guildId, db ->
+			db.proficiencyScope.getLanguages(guildId.toString()).toList()
+		}
 
-        cacheManager.createNewCollectionCache<ToolProficiency> { guildId, db ->
-            db.proficiencyScope.getToolProficiencies(guildId.toString()).toList()
-        }
+		cacheManager.createNewCollectionCache<ToolProficiency> { guildId, db ->
+			db.proficiencyScope.getToolProficiencies(guildId.toString()).toList()
+		}
 
-        launch {
-            val gateway = ExternalGateway(cacheManager)
-            gateway.open()
-        }
+		launch {
+			val gateway = ExternalGateway(cacheManager)
+			gateway.open()
+		}
 
-        kord.login {
-            intents += Intent.GuildMembers
-            intents += Intent.GuildPresences
-            intents += Intent.MessageContent
-        }
-    }
+		kord.login {
+			intents += Intent.GuildMembers
+			intents += Intent.GuildPresences
+			intents += Intent.MessageContent
+		}
+	}
 
 }
 
 suspend fun main() {
-    val kord = Kord(System.getenv("BOT_TOKEN")!!)
-    val bot = KaironBot("wagham", kord)
-    bot.start()
+	val kord = Kord(System.getenv("BOT_TOKEN")!!)
+	val bot = KaironBot("wagham", kord)
+	bot.start()
 }
